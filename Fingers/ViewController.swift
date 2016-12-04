e25b113e3b4c8d3ab2169f3a76621f04ea3c837a
prkashi <punithrkashi@gmail.com>
2016-12-04 16:29:34 -0500

    First Commit

--- Fingers/ViewController.swift
+++ Fingers/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var result: UILabel!
+    
+    @IBOutlet var age: UITextField!
 
+    @IBAction func submit(_ sender: Any) {
+        
+        
+        //get the value from the text field
+        
+        let ageinnumber  = age.text
+        
+        //generate random number
+        
+        let random = String(arc4random_uniform(4) + 1)
+
+        //checking for the equality
+        
+        if (age.text?.isEmpty) == true{
+         
+            result.text = "Please enter a number"
+            result.alpha = 1.0
+            result.textColor = UIColor.brown
+            
+            
+        } else if random == ageinnumber {
+            
+            result.text = "You are right!!!"
+            result.alpha = 1.0
+            result.textColor = UIColor.green
+            
+        } else{
+            
+            result.text = "You are wrong, I was holding \(random)"
+            result.alpha = 1.0
+            result.textColor = UIColor.red
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //hide the result label
+        
+        result.alpha = 0.00
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
